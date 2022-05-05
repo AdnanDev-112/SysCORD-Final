e2bfe8318b9007b1a041ee87360230801641b954
Adnan112-dev <79618300+Adnan112-dev@users.noreply.github.com>
2022-05-05 17:26:04 +0530

    Formatting left maybe

--- lib/modules/attendance/Total_all_subjects/excsheet.dart
+++ lib/modules/attendance/Total_all_subjects/excsheet.dart
@@ -6,6 +6,14 @@ import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:syncfusion_flutter_xlsio/xlsio.dart';
 
+// Gen Letters
+var aCode = 'A'.codeUnitAt(0);
+var zCode = 'Z'.codeUnitAt(0);
+List<String> alphabetLetters = List<String>.generate(
+  zCode - aCode + 1,
+  (index) => String.fromCharCode(aCode + index),
+);
+
 Future<void> createTotalExcel(var dataRecieved) async {
 // Clean the Data
   var _selectedMonth = dataRecieved['Month'];
@@ -14,7 +22,8 @@ Future<void> createTotalExcel(var dataRecieved) async {
   var subjectsTotal = dataRecieved['attendanceData'];
   var classNumbers = dataRecieved['classRange'];
 
-  print(subjectsTotal);
+  // print(subjectsTotal);
+  print(dataRecieved);
 
   //
   final Workbook workbook = Workbook();
@@ -28,16 +37,52 @@ Future<void> createTotalExcel(var dataRecieved) async {
   sheet.getRangeByName("C1").setText(selectedClass);
   sheet.getRangeByName("D1").setText(_selectedMonth);
   sheet.getRangeByName("E1").setText(_selectedYear);
+
   // ****************************
   // Set Init
   for (int i = 0; i < classNumbers.length; i++) {
-    var rowNumber = (3 + i).toString();
+    var rowNumber = (4 + i).toString();
     sheet.getRangeByName("A" + rowNumber).setText(classNumbers[i].toString());
   }
   // Adv
   for (int i = 0; i < subjectsTotal.length; i++) {
     var subjectName = subjectsTotal[i]['className'];
     var classesConducted = subjectsTotal[i]['classesConducted'];
+    subjectsTotal[i].remove('className');
+    subjectsTotal[i].remove('classesConducted');
+    // print(subjectsTotal[i]);
+    var attendedDays = [];
+    subjectsTotal[i].forEach((key, value) {
+      attendedDays.add(value);
+    });
+    // sheet.getRangeByName("B3").setText(subjectName);
+    // sheet.getRangeByName("D3").setText(subjectName);
+    // sheet.getRangeByName("B4").setText('Attendance');
+    // sheet.getRangeByName("C4").setText('Conducted');
+    // *************************************************************************
+    var letter1 = alphabetLetters[2 + i * 3 - 1]; // Letter B
+    var letter2 = alphabetLetters[3 + i * 3 - 1]; // Letter C
+    var letter3 = alphabetLetters[4 + i * 3 - 1]; // Letter D
+    // print(letter1);
+    // print(letter2);
+    // print(letter3);
+    sheet.getRangeByName("${letter1}2").setText(subjectName);
+    sheet.getRangeByName("${letter3}2").setText('Percentage');
+    sheet.getRangeByName("${letter1}3").setText('Attendance');
+    sheet.getRangeByName("${letter2}3").setText('Conducted');
+
+    for (int j = 0; j < classNumbers.length; j++) {
+      var rowNumber = (4 + j).toString();
+      sheet
+          .getRangeByName(letter1 + rowNumber)
+          .setText(attendedDays[j].toString());
+      sheet
+          .getRangeByName(letter2 + rowNumber)
+          .setText(classesConducted.toString());
+      sheet
+          .getRangeByName(letter3 + rowNumber)
+          .setFormula('=$letter1$rowNumber/$letter2$rowNumber * 100');
+    }
   }
 
   final List<int> bytes = workbook.saveAsStream();
